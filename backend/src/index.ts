080f1d0119d8d32f8f56ff1c12c21fc703c94854
Hussnain Raza <hussnain444all@gmail.com>
2025-10-15 23:31:30 +0500

    cors update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,6 +26,7 @@ app.use(helmet());
 app.use(cors({
     origin: [
         process.env.FRONTEND_URL || 'http://localhost:3000',
+        "https://scrapify-backend.vercel.app"
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
